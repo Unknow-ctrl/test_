1f0ec863ee27b9d4f9d2df44c4c03602e95c78a2
Korbi <huber_korbinian06@gmx.de>
2024-07-07 12:08:18 +0200

    Test_erste_Datei

---
+++ hello_world.py
@@ -0,0 +1,40 @@
+rufname ="Korbinian Huber"
+print("Hello World")
+print(rufname)
+print("5+5")
+print("10**2")
+
+print("time")
+print(rufname)
+# Variable: Platzhalter für Informationen
+#1.Variablennamen
+# Information wird von rechts nach links kopiert
+# = Zuweisungsoperator
+y = 8 # y ist Platzhalter, Variable
+x = 6 # Definition einer Variable x
+
+#Rechnen mit Variablen 
+y = y+5 
+
+#1. Berechnung der Summe auf der rechten Seite
+#2. Zuweisung an y -> y wird überschrieben
+
+print("Das steht in der Variable y drin:")
+print(y) #In y steht jetzt 13 drin.
+
+y=y+x #y ist jetzt 19
+print("Das steht nach der Summe aus y+x")
+print(y)
+
+y=y-5 #y=19-5 y ist jetzt 14
+print(y)
+
+y=5 #Integer , Ganzzahl
+g = 1.5 # Float, (Fließ-)Kommazahl
+h = True #Boolean,Wahrheitswert
+h = False
+z= "" #Das ist eine Zeichkette, String
+alter =input("Bitte alter eingeben:")
+print("Du hast folgendes alter:")
+print(alter)
+
